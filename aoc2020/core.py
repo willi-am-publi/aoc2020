02a27714ccda024a21da0a9bc86925b22124d3ca
Will D <willi.am.publi@gmail.com>
2020-12-01 22:05:42 +0100

    Add docstrings

--- aoc2020/core.py
+++ aoc2020/core.py
@@ -3,6 +3,9 @@ import itertools as it
 import typing as typ
 
 class ExpenseReport():
+    """
+    Class that holds attributes and methods related to Expense Report
+    """
 
     @classmethod
     def _input_reader(
@@ -10,6 +13,25 @@ class ExpenseReport():
         file_path: str,
         linesep: typ.Optional[str] = "\n"
     ) -> typ.List[int]:
+        """
+        Reads text file containing numbers
+
+        Args:
+            file_path (str): 
+                Path to file
+            linesep (typ.Optional[str], optional): 
+                Line separator. Defaults to "\n".
+
+        Returns:
+            typ.List[int]: List of integers
+
+        :Example:
+            >>> er = ExpenseReport._input_reader(
+            >>>     'tests/inputs/day1'
+            >>> )
+            >>> type(er)
+            list
+        """
         with open(file_path) as file:
             inputs = file.readlines()
             inputs = [raw_str.rstrip(linesep) for raw_str in inputs]
@@ -21,6 +43,25 @@ class ExpenseReport():
         file_path: str,
         r: typ.Optional[int] = 2
     ) -> typ.Tuple[int, ...]:
+        """Founds integers which sums equals to 2020
+
+        Args:
+            file_path (str):
+                File path
+            r (typ.Optional[int], optional):
+                Number of integers to sum up. Defaults to 2.
+
+        Returns:
+            typ.Tuple[int, ...]: 
+                Tuple of integers, number of integers depends on r
+
+        :Example:
+            >>> ExpenseReport._sum_2020_getter(
+            >>>     'tests/inputs/day1'
+            >>> )
+            (1721, 299)
+
+        """
         target = 2020
         expense_report = cls._input_reader(file_path=file_path)
         for combination in it.permutations(
@@ -36,6 +77,22 @@ class ExpenseReport():
         file_path: str,
         r: typ.Optional[int] = 2
     ) -> int:
+        """[summary]
+
+        Args:
+            file_path (str): [description]
+            r (typ.Optional[int], optional): [description]. Defaults to 2.
+
+        Returns:
+            int: [description]
+
+        :Example:
+            >>> ExpenseReport.product_sum_equals_2020(
+            >>>     'tests/inputs/day1'
+            >>> )
+            514579
+
+        """
         nums = cls._sum_2020_getter(
             file_path=file_path,
             r=r
