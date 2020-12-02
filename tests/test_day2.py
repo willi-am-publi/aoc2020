771ee9640b2ffa4f8653ee68404cc6d6b64b36b3
Will D <willi.am.publi@gmail.com>
2020-12-03 00:47:29 +0100

    Add TestPasswordDB

--- tests/test_day2.py
+++ tests/test_day2.py
@@ -2,6 +2,7 @@ import os
 import pytest as pt
 
 import aoc2020.day2 as d2
+import tests.test_common as tc
 
 
 class TestPassword():
@@ -28,3 +29,37 @@ class TestPassword():
             entry=e
         )
         assert password.is_valid == iv
+
+
+class TestPasswordDB():
+
+    class_ = d2.PasswordDB
+    file = os.path.join(tc.TESTINPUTDIR, 'day2')
+
+    def test_file_reader_is_list(self):
+        content = self.class_._file_reader(
+            file_path = self.file
+        )
+        assert isinstance(content, list) == True
+
+    __parsed_entry = [
+            (1, 3, "a", "abcde"),
+            (1, 3, "b", "cdefg"),
+            (2, 9, "c", "ccccccccc")
+    ]
+
+    def test_parse_input(self):
+        assert self.class_._parse_input(
+            i = "1-3 a: abcde"
+        ) == self.__parsed_entry[0]
+
+    def test_input_reader(self):
+        input_ = self.class_._input_reader(
+            file_path = self.file
+        )
+        assert input_ == self.__parsed_entry
+
+    def test_valid_counter(self):
+        assert self.class_.valid_counter(
+            file_path = self.file
+        ) == 2
