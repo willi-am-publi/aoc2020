cbfc572cf5004aa902c5494660ba2f2158320c06
Will D <willi.am.publi@gmail.com>
2020-12-03 17:48:42 +0100

    Add docstrings

--- aoc2020/day2.py
+++ aoc2020/day2.py
@@ -7,6 +7,24 @@ def sled_policy(
     letter:str,
     entry:str,
 ) -> bool:
+    """
+    Reflects sled company password policy
+
+    Args:
+        first_num (int):
+            letter occurences lower limit
+        second_num (int):
+            letter occurences upper limit
+        letter (str):
+            letter of interest
+        entry (str):
+            password entry
+
+    Returns:
+        bool: 
+            whether or not entry conforms to sled company
+            password policy
+    """
     matches = len(re.findall(
         letter,
         entry
@@ -19,6 +37,24 @@ def toboggan_policy(
     letter:str,
     entry:str,
 ) -> bool:
+    """
+    Reflects toboggan company password policy
+
+    Args:
+        first_num (int):
+            first index letter matching check in entry
+        second_num (int):
+            second index letter matching check in entry
+        letter (str):
+            letter of interest
+        entry (str):
+            password entry
+
+    Returns:
+        bool: 
+            whether or not entry conforms to toboggan company
+            password policy
+    """
     return ((entry[first_num-1] == letter)
             != (entry[second_num-1] == letter)
     )
@@ -38,10 +74,14 @@ class Password():
         """s/e
 
         Args:
-            first_num (int): letter count lower limit
-            second_num (int): letter count upper limit
-            letter (str): letter of interest
-            entry (str): (possibbly corrupted) entry password
+            first_num (int):
+                first condition, interpretation depends on policy
+            second_num (int):
+                first condition, interpretation depends on policy
+            letter (str):
+                letter of interest
+            entry (str):
+                (possibbly corrupted) entry password
         """
         self.kwargs = {
             "first_num": first_num,
@@ -69,15 +109,18 @@ class PasswordDB():
         file_path: str,
         linesep: typ.Optional[str] = "\n"
     ) -> typ.List[str]:
-        """[summary]
+        """Reads input file
 
         Args:
             file_path (str):
                 Path to file
-            linesep (typ.Optional[str], optional): [description]. Defaults to "\n".
+            linesep (typ.Optional[str], optional): 
+                Line separator. Defaults to "\n".
 
         Returns:
-            typ.List[str]: [description]
+            typ.List[str]:
+                List of inputs, stripped from surrounding spaces
+                and line separators 
         """
         with open(file_path) as file:
             return [raw_str.strip().rstrip(linesep) for raw_str
@@ -105,15 +148,15 @@ class PasswordDB():
         file_path: str,
     ) -> typ.List[typ.Tuple[int, int, str, str]]:
         """
-        [summary]
+        Parses and formats entries to tuples
 
         Args:
             file_path (str):
-                [description]
+                File path
 
         Returns:
             typ.List[typ.Tuple[int, int, str, str]]:
-                [description]
+                Tuples holding entries informations
         """
         content = cls._file_reader(
             file_path=file_path
@@ -124,9 +167,24 @@ class PasswordDB():
     def valid_counter(
         cls,
         file_path: str,
+        policy: typ.Optional[typ.Callable] = sled_policy
         ) -> int:
+        """
+        Counts number of entries conforming to policy
+
+        Args:
+            file_path (str):
+                File path
+            policy (typ.Optional[typ.Callable], optional):
+                Policy to use. Defaults to sled_policy.
+
+        Returns:
+            int: Number of valid entries
+        """
         entries = cls._input_reader(
-            file_path = file_path
+            file_path = file_path            
         )
-        return sum([Password(*entry).is_valid()
-                    for entry in entries])
+        return sum([Password(*entry).is_valid(
+                policy=policy
+            ) for entry in entries]
+        )
